3bfaf088c36e1a4a1fe85ab92324fd48c59972f7
AcheZeta <ache.mariana@gmail.com>
2019-10-12 13:27:07 -0500

    SET: StackView

---
+++ PersonalityQuiz-ECC/PersonalityQuiz-ECC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz-ECC
+//
+//  Created by Laboratoria on 10/12/19.
+//  Copyright © 2019 equipo5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
