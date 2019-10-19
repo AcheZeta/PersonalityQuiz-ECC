39d8a5a4eb3b53cda312f9a29e45ef9b8d1c1bb5
AcheZeta <ache.mariana@gmail.com>
2019-10-19 14:02:35 -0500

    Answer Structure with Question

--- PersonalityQuiz-ECC/PersonalityQuiz-ECC/QuestionViewController.swift
+++ PersonalityQuiz-ECC/PersonalityQuiz-ECC/QuestionViewController.swift
@@ -17,5 +17,17 @@ class QuestionViewController: UIViewController {
     }
     
    // var questions: Array = ["En que pais te gustaria vivir?", "Con que te gusta combinar tu pan", "Que tan dulce prefieres el pan?"]
-    var questions = [Question(text: "En que pais te gustaria vivir?", type: .single, answer: [Answer(text: "Francia", type: .PanCuernito)])]
+    var questions = [Question(text: "En que pais te gustaria vivir?", type: .single, answer: [Answer(text: "Francia", type: .PanCuernito),
+        Answer(text: "USA", type: .PanAJo),
+        Answer(text: "Alemania", type: .PanCenteno),
+        Answer(text: "México", type: .PanMuerto)]),
+        Question(text: "Con qué te gusta combinar tu pan", type: .multiple, answer:
+            [Answer(text: "Leche", type: .PanMuerto),
+            Answer(text: "Jamón y Queso", type: .PanCuernito),
+            Answer(text: "Vino", type: .PanCenteno),
+            Answer(text: "Pastas", type: .PanAJo),
+        ]),
+        Question(text: "Qué prefieres", type: .ranged, answer: [Answer(text: "Dulce", type: .PanMuerto),
+        Answer(text: "Salado", type: .PanAJo)])
+    ]
 }
